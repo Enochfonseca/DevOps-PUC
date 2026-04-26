75e1caa741dbf963ba628f9fbd2c4c6e9b52adb9
Enoch Fonseca <enochfonsecajr@gmail.com>
2026-04-26 11:04:05 -0300

    docker-file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import random
-import random
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -17,19 +15,19 @@ class Estudante(BaseModel):
 @app.get("/helloworld")
 async def root():
     return {"message": "Hello World"}
-@app.get("/funcaoteste}")
+
+@app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 30000)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 5000)}
 
 @app.post("/estudantes/cadastro")
 async def create_estudante(estudante: Estudante):
     return estudante
-@app.put("/estudantes/update/{id_estudante]")
+
+@app.put("/estudantes/update/{id_estudante}")
 async def update_item(id_estudante: int):
     return id_estudante > 0
 
 @app.delete("/estudantes/delete/{id_estudante}")
 async def delete_estudante(id_estudante: int):
     return id_estudante > 0
-
-
